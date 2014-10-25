93812688354f030cde5b67ebcb48f85433fb3bff
Leandro <grilo106@gmail.com>
2014-10-25 13:36:08 -0200

    Alguns DAO + Layout

--- src/br/com/trampolinbrank/dao/UsuarioDAO.java
+++ src/br/com/trampolinbrank/dao/UsuarioDAO.java
@@ -2,11 +2,7 @@ package br.com.trampolinbrank.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-
 import br.com.trampolinbank.bean.Usuario;
 import br.com.trampolinbrank.factory.ConnectionFactory;
 
